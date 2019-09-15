9fb38a9f7b7533ec710459cb132b63f848332497
Le Duy Long <longhust9x@gmail.com>
2019-09-15 14:29:16 +0700

    fix auto link react-native-vector-icons

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -5,6 +5,8 @@ import Text from './Text';
 import Block from './Block';
 import Button from './Button';
 import { theme } from '../constants';
+import Icon from 'react-native-vector-icons/Ionicons';
+Icon.loadFont();
 
 export default class Input extends Component {
   state = {
@@ -32,15 +34,14 @@ export default class Input extends Component {
         style={styles.toggle}
         onPress={() => this.setState({ toggleSecure: !toggleSecure })}
       >
-        {/* {
+        {
           rightLabel ? rightLabel :
-            <Icon.Ionicons
+            <Icon
               color={theme.colors.gray}
               size={theme.sizes.font * 1.35}
               name={!toggleSecure ? "md-eye" : "md-eye-off"}
           />
-        } */}
-        <Text>Button</Text>
+        }
       </Button>
     );
   }
